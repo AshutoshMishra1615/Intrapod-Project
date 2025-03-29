d47baf0232898cd2f464f86e7e6a0ecec86004b2
Chitrangi Kasliwal <chitrangikasliwal@Chitrangis-MacBook-Air.local>
2025-03-29 15:14:50 +0530

    feat (ui): add LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './App.css'
+import { LoadingScreen } from './components/LoadingScreen';
+import "./index.css";
+import { MobileMenu } from './components/MobileMenu';
+import { Navbar } from './components/Navbar';
+import  Home  from './components/sections/Home';
+import { Footer } from './components/Footer';
+
 
 function App() {
-
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
-     
-    </>
-  )
+  {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+    <div className= {`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-custom-gradient text-gray-100`} >
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
+      <Footer />
+      
+
+    </div>
+  </>
+  );
+  
 }
 
 export default App
